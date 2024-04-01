b082a4cfef271f8397c53105d231fdfae7e0684d
Martin Ronning <ronningmartin@outlook.com>
2024-04-01 16:02:20 +0200

    close bracket

--- ga.py
+++ ga.py
@@ -5,7 +5,6 @@ def init(n):
     print("init")
     population = create_population(n)
     print("pop. created")
-    print(population)
 
 def create_population(n):
     population = np.empty(0)
@@ -20,5 +19,5 @@ def selection():
     print("selection")
 
 def tournament():
-    print("tournament)"
+    print("tournament")
 
